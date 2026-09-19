33060588999846a2454a752cce8168c71d71fca0
Heran Guan <heran_guan@apple.com>
2026-09-19 16:50:35 -0500

    Point dim_product and dim_address at staging models with renamed columns

--- adventureworks/models/marts/dim_address.sql
+++ adventureworks/models/marts/dim_address.sql
@@ -1,24 +1,24 @@
 with stg_address as (
     select *
-    from {{ ref('address') }}
+    from {{ ref('stg_person__address') }}
 ),
 
 stg_stateprovince as (
     select *
-    from {{ ref('stateprovince') }}
+    from {{ ref('stg_person__stateprovince') }}
 ),
 
 stg_countryregion as (
     select *
-    from {{ ref('countryregion') }}
+    from {{ ref('stg_person__countryregion') }}
 )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['stg_address.addressid']) }} as address_key,
-    stg_address.addressid,
-    stg_address.city as city_name,
-    stg_stateprovince.name as state_name,
-    stg_countryregion.name as country_name
+    {{ dbt_utils.generate_surrogate_key(['stg_address.address_id']) }} as address_key,
+    stg_address.address_id,
+    stg_address.city_name,
+    stg_stateprovince.state_name,
+    stg_countryregion.country_name
 from stg_address
-left join stg_stateprovince on stg_address.stateprovinceid = stg_stateprovince.stateprovinceid
-left join stg_countryregion on stg_stateprovince.countryregioncode = stg_countryregion.countryregioncode
+left join stg_stateprovince on stg_address.state_province_id = stg_stateprovince.state_province_id
+left join stg_countryregion on stg_stateprovince.country_region_code = stg_countryregion.country_region_code
--- adventureworks/models/marts/dim_product.sql
+++ adventureworks/models/marts/dim_product.sql
@@ -1,27 +1,27 @@
 with stg_product as (
     select *
-    from {{ ref('product') }}
+    from {{ ref('stg_production__product') }}
 ),
 
 stg_product_subcategory as (
     select *
-    from {{ ref('productsubcategory') }}
+    from {{ ref('stg_production__productsubcategory') }}
 ),
 
 stg_product_category as (
     select *
-    from {{ ref('productcategory') }}
+    from {{ ref('stg_production__productcategory') }}
 )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['stg_product.productid']) }} as product_key,
-    stg_product.productid,
-    stg_product.name as product_name,
-    stg_product.productnumber,
-    stg_product.color,
-    stg_product.class,
-    stg_product_subcategory.name as product_subcategory_name,
-    stg_product_category.name as product_category_name
+    {{ dbt_utils.generate_surrogate_key(['stg_product.product_id']) }} as product_key,
+    stg_product.product_id,
+    stg_product.product_name,
+    stg_product.product_number,
+    stg_product.product_color,
+    stg_product.product_class,
+    stg_product_subcategory.product_subcategory_name,
+    stg_product_category.product_category_name
 from stg_product
-left join stg_product_subcategory on stg_product.productsubcategoryid = stg_product_subcategory.productsubcategoryid
-left join stg_product_category on stg_product_subcategory.productcategoryid = stg_product_category.productcategoryid
+left join stg_product_subcategory on stg_product.product_subcategory_id = stg_product_subcategory.product_subcategory_id
+left join stg_product_category on stg_product_subcategory.product_category_id = stg_product_category.product_category_id
